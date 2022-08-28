c043df2dc5b515785e6eeaa0ac6399e1f2bf9d9a
arvind9140 <ar789maurya@gmail.com>
2022-08-28 20:30:34 +0530

    initial commit

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,55 @@
+import React from "react"
+import Back from "../common/back/Back"
+import "./contact.css"
+
+const Contact = () => {
+  const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14283.299474974337!2d80.27709693004029!3d26.493583132956424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399c3953ccf08ed7%3A0xa5c2ee064e1dc6f7!2sKanpur%20City%20Hospital!5e0!3m2!1sen!2sin!4v1661243787447!5m2!1sen!2sin"  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  return (
+    <>
+      <Back title='Contact us' />
+      <section className='contacts padding'>
+        <div className='container shadow flexSB'>
+          <div className='left row'>
+            <iframe src={map}></iframe>
+          </div>
+          <div className='right row'>
+            <h1>Contact us</h1>
+            <p>We're open for any suggestion or just to have a chat</p>
+
+            <div className='items grid2'>
+              <div className='box'>
+                <h4>ADDRESS:</h4>
+                <p>247, front of Shakuntala Nikunj, Vikas Nagar, Kanpur,  India</p>
+              </div>
+              <div className='box'>
+                <h4>EMAIL:</h4>
+                <p> info@arvi789.com</p>
+              </div>
+              <div className='box'>
+                <h4>PHONE:</h4>
+                <p> +91 8188053879</p>
+              </div>
+            </div>
+
+            <form action=''>
+              <div className='flexSB'>
+                <input type='text' placeholder='Name' />
+                <input type='email' placeholder='Email' />
+              </div>
+              <input type='text' placeholder='Subject' />
+              <textarea cols='30' rows='10'>
+                Create a message here...
+              </textarea>
+              <button className='primary-btn'>SEND MESSAGE</button>
+            </form>
+
+            <h3>Follow us here</h3>
+            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Contact
